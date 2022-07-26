0aa41217883359e920c2b07c482aaaade455df1d
jdcera4 <jdcera4@gmail.com>
2022-07-26 14:54:24 -0500

    create enviroment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { AccordionCollapse } from "react-bootstrap";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAlKjd-oUlU8sn6_XMqmUGSLxMRKRRtJvk",
-  authDomain: "portal-usuarios-92d1a.firebaseapp.com",
-  projectId: "portal-usuarios-92d1a",
-  storageBucket: "portal-usuarios-92d1a.appspot.com",
-  messagingSenderId: "597443558368",
-  appId: "1:597443558368:web:d19eb72df08bb9d04bc985"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 };
 
 // Initialize Firebase
